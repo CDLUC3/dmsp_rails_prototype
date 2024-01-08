c0f8117ff043fc96951486e30e835cf6ea499e5a
briri <brian.riley@ucop.edu>
2024-01-08 11:15:26 -0800

    update to buildspec, dockerfile. created development creds. Added test that will fail to test AWS codebuild

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -4,4 +4,8 @@ class TagTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'Verify that the Rails app can load itself' do
+    puts "HOST: #{Rails.configuration.action_mailer.default_url_options[:host]}"
+    asset 1 == 0
+  end
 end
